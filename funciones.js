6ecb4da2ee911f9abd1b73a604098bf893996a32
Strockersoft <stkpablodiaz@gmail.com>
2022-10-08 21:34:15 -0300

    video9 curso gas-avanzado

--- funciones.js
+++ funciones.js
@@ -3,6 +3,13 @@ function doGet() {
 }
 
 
+function obtenerContactos(){
+    let hoja  = SpreadsheetApp.openById('1W4sBpAxAa8P1HYkLpmeFmyd3vVof_HL_OgCLKjybcmc').getActiveSheet();
+    let datos = hoja.getDataRange().getValues();
+    return datos;
+}
+
+
 
 function include(archivo) {
     return HtmlService.createHtmlOutputFromFile(archivo).getContent();
