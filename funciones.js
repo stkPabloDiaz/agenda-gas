448bd085ac71ea9cf3dc53cd0bc047c79f91a2da
Strockersoft <stkpablodiaz@gmail.com>
2022-10-10 23:28:59 -0300

    video15 curso-gas-avanzado

--- funciones.js
+++ funciones.js
@@ -1,15 +1,23 @@
+const HOJA = SpreadsheetApp.openById('1W4sBpAxAa8P1HYkLpmeFmyd3vVof_HL_OgCLKjybcmc').getActiveSheet();
+
 // Primer funcion a ejecutarse a cargar el Proyecto
 function doGet() {
     return HtmlService.createTemplateFromFile('web').evaluate().setTitle('stkAgenda');
 }
 
-
+function doPost(datos) {
+    return HtmlService.createTemplateFromFile('web').evaluate().setTitle('stkAgenda');
+}
 
 function obtenerContactos(){
-    let hoja  = SpreadsheetApp.openById('1W4sBpAxAa8P1HYkLpmeFmyd3vVof_HL_OgCLKjybcmc').getActiveSheet();
-    let datos = hoja.getDataRange().getValues();
-    return datos;
+    return HOJA.getDataRange().getValues();
 }
+
+// Funcion para Agregar Contacto
+function insertarContacto(nombre, correo) {
+    HOJA.appendRow([nombre,correo]);
+}
+
 
 
 
